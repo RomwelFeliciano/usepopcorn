5018c46dd20852901080a059640cef485be8308c
elciddelapaz <127919521+RomwelFeliciano@users.noreply.github.com>
2024-04-10 15:45:56 +0800

    Finished Star Rating

--- src/index.js
+++ src/index.js
@@ -4,11 +4,32 @@ import ReactDOM from 'react-dom/client';
 // import App from "./App";
 
 import StartRating from './StartRating';
+import { useState } from 'react';
+
+function Test() {
+	const [movieRating, setMovieRating] = useState(0);
+
+	return (
+		<div>
+			<StartRating
+				color="blue"
+				maxRating={8}
+				onSetRating={setMovieRating}
+			/>
+			<p>This movies was rated {movieRating} stars</p>
+		</div>
+	);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		{/* <App /> */}
-		<StartRating />
+		<StartRating
+			messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+		/>
+		<StartRating color="red" size={28} className="test" defaultRating={3} />
+
+		<Test />
 	</React.StrictMode>
 );
